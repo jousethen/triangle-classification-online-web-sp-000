72a30466d32cdc708e8d9b9f71d9e5600f802453
Jouse then <jthen93@gmail.com>
2021-03-04 19:46:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
     if valid? == false
       begin
         raise TriangleError
-        rescue TriangleError => error
+      rescue TriangleError => error
           puts error.message
       end
     elsif @s1 == @s2 && @s1 == @s3
