24c2ea57149cfe458b3373b501bcd7aeb477887f
Jouse then <jthen93@gmail.com>
2021-03-04 19:41:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,18 @@ class Triangle
     @s3 = s3
   end
   
+  def valid?
+    if (@s1 + @s2 < @s3 || @s1 + @s3 < @s2 || @s2 + @s3 < @s1 || @s1 == 0 || @s2 == 0 || @s3 == 0)
+      return false 
+    else
+      return true 
+    end
+  end
+  
   def kind
-    if @s1 == @s2 && @s1 == @s3
+    if valid? == false
+      raise TriangleError
+    elsif @s1 == @s2 && @s1 == @s3
       return :equilateral
     elsif @s1 != @s2 && @s2 != @s3 && @s1 != @s3
       return :scalene
@@ -16,4 +26,9 @@ class Triangle
       return :isosceles
     end
   end
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
+  
 end
